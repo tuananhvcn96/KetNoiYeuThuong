33821cbbf4527bb06f8d124aee52f97bbf613598
Tuan Anh <tuananh5871@gmail.com>
2018-06-22 23:06:44 +0700

    Test

---
+++ KetNoiYeuThuong/KetNoiYeuThuong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KetNoiYeuThuong
+//
+//  Created by Tuan Anh on 6/22/18.
+//  Copyright © 2018 Tuan Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
